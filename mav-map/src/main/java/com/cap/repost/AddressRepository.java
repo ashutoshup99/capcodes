8c35d925fcd16e4e6032d464d9f5dbd1f03c7278
ashutoshup99 <ashuup99@gmail.com>
2021-02-20 10:39:35 +0530

    Second Commit

--- mav-map/src/main/java/com/cap/repost/AddressRepository.java
+++ mav-map/src/main/java/com/cap/repost/AddressRepository.java
@@ -1,6 +1,15 @@
 package com.cap.repost;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class AddressRepository {
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("capdbjpa");
+
+	
+	/* Create EntityManager */
+	EntityManager em = emf.createEntityManager();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- mav-map/src/main/java/com/cap/repost/PersonRepository.java
+++ mav-map/src/main/java/com/cap/repost/PersonRepository.java
@@ -1,7 +1,15 @@
 package com.cap.repost;
 
-public class PersonRepository {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+public class PersonRepository {
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("capdbjpa");
+
+	
+	/* Create EntityManager */
+	EntityManager em = emf.createEntityManager();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
