bf988b32687107c7c84bc2ac7cdb4465138c0f54
ashutoshup99 <ashuup99@gmail.com>
2021-02-22 10:57:49 +0530

    Spring basics

---
+++ cap-spring/src/main/java/com/cap/UserRepository.java
@@ -0,0 +1,10 @@
+package com.cap;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserRepository {
+	public void findUserById() {
+		System.out.println("finding a user by its id");
+	}
+}
---
+++ cap-spring/src/main/java/com/cap/WorkoutRepository.java
@@ -0,0 +1,20 @@
+package com.cap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WorkoutRepository {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+//	public WorkoutRepository(UserRepository userRepository) {
+//		this.userRepository = userRepository;
+//	}
+	
+	public void assignWorkoutToUser() {
+		userRepository.findUserById();
+		System.out.println("assigning workout to user");
+	}
+}
